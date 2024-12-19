405b7e4d7c10cb3aa91ad53a07decd2568f7f05e
Jllaneras <jonathanl1020.jl@gmail.com>
2024-12-18 21:48:28 -0500

    Added two new components and started building navbar

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="Home">Home</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+        <li>
+          <a href="Menu">Menu</a>
+        </li>
+        <li>
+          <a href="Online Reservation">Online Reservation</a>
+        </li>
+        <li>
+          <a href="Order Online">Order Online</a>
+        </li>
+        <li>
+          <a href="Login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+export default function Header() {
+  return (
+    <nav>
+      <img src="" alt=""></img>
+      <ul>
+        <li>
+          <a href="Home">Home</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+        <li>
+          <a href="Menu">Menu</a>
+        </li>
+        <li>
+          <a href="Online Reservation">Online Reservation</a>
+        </li>
+        <li>
+          <a href="Order Online">Order Online</a>
+        </li>
+        <li>
+          <a href="Login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
